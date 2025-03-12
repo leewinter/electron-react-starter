c061d44d808e86f7fee370985a2a4af0e5461ce1
Lee Winter <lee.winter@comgroupltd.com>
2025-03-12 18:38:36 +0000

    Fix monaco loader in built version

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,12 +1,12 @@
+import * as monaco from 'monaco-editor';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouterComponent from './router-component';
 import { loader } from '@monaco-editor/react';
 
 loader.config({
-  paths: {
-    vs: 'app-asset://zui/node_modules/monaco-editor/min/vs',
-  },
+  monaco,
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
